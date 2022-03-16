631bfdf35f1b8507f910f0bf274ecb931c6eda66
faithon <ook503hk@gmail.com>
2022-03-15 18:41:27 -0700

    frontend setup

---
+++ src/frontend/components/Home.js
@@ -0,0 +1,12 @@
+import { useState, useEffect } from 'react'
+import { ethers } from "ethers"
+import { Row, Col, Card, Button } from 'react-bootstrap'
+
+const Home = ({ marketplace, nft }) => {
+    return (
+        <div className="flex justify-center">
+        </div>
+    );
+}
+
+export default Home
